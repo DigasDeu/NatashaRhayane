45bfbc18027cf9e5616ea8dc026d427dae3fa951
DigasDeu <diegofernandosilva.10@gmail.com>
2025-06-10 14:02:09 -0400

    first commit

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('Felix Aniversaio Nathy').split('')
+    const titles = ('Feliz Aniversaio Nathy').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
